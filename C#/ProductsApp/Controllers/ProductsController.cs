7007c9f7f6525e81531532dc295c41cafa072a79
fclerget <franckjunior.clerget@gmail.com>
2018-10-20 13:43:23 -0400

    [project-creation] create project
    add simple UI
    add starting API backend
    add tcp connection to refresh UI

---
+++ C#/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using ProductsApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using PusherServer;
+using System.Threading.Tasks;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        [HttpGet]
+        [ActionName("products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [HttpGet]
+        [ActionName("product")]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet]
+        [ActionName("helloword")]
+        public async Task<IHttpActionResult> GetHelloWord()
+        {
+            var options = new PusherOptions
+            {
+                Cluster = "mt1",
+                Encrypted = true
+            };
+
+            var pusher = new Pusher(
+              "626583",
+              "05b6d74f8a0e925376a1",
+              "fed1a216748aa764f8bf",
+              options);
+
+            var result = await pusher.TriggerAsync(
+              "my-channel",
+              "my-event",
+              new { message = "hello world" });
+
+            return Ok();
+        }
+    }
+}
---
+++ C#/ProductsApp/UI/js/chess-client-js/chess-client.js
@@ -0,0 +1,52 @@
+﻿
+var GET_PRODUCTS = "api/products/products/";
+
+function getProducts(htmlId) {
+
+    $.getJSON(GET_PRODUCTS)
+        .done(function (data) {
+            // On success, 'data' contains a list of products.
+            $.each(data, function (key, item) {
+                // Add a list item for the product.
+                $('<li>', { text: formatItem(item) }).appendTo($('#' + htmlId));
+            });
+        });
+}
+
+var GET_PRODUCT = "api/products/product/";
+
+function getProduct(inputId, outputId) {
+
+    var id = $('#' + inputId).val();
+    
+    $.getJSON(GET_PRODUCT + id)
+        .done(function (data) {
+            $('#' + outputId).text(formatItem(data));
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $('#' + outputId).text('Error: ' + err);
+        });
+}
+
+function formatItem(item) {
+    return item.Name + ': $' + item.Price;
+}
+
+var CHANNEL_NAME = "my-channel";
+var EVENT_NAME = "my-event";
+
+function subscribe() {
+
+    Pusher.logToConsole = true;
+
+    var pusher = new Pusher('05b6d74f8a0e925376a1', {
+        cluster: 'mt1',
+        forceTLS: true
+    });
+
+    var channel = pusher.subscribe(CHANNEL_NAME);
+
+    channel.bind(EVENT_NAME, function (data) {
+        alert(JSON.stringify(data));
+    });
+}
